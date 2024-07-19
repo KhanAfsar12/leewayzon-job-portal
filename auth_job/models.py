48dd11a4c9826ad0141198c86cd0e59034179c3c
KhanAfsar12 <ka484564@gmail.com>
2024-07-19 17:00:31 +0530

    Optimize applicatin

--- auth_job/models.py
+++ auth_job/models.py
@@ -8,14 +8,14 @@ class Job(models.Model):
     job_type = models.CharField(max_length=200)
     job_duration = models.CharField(max_length=200)
     job_sector = models.CharField(max_length=200)
-    job_description = models.CharField(max_length=500)
+    job_description = models.TextField()
     job_location = models.CharField(max_length=200)
-    company_description = models.CharField(max_length=1000)
-    responsibilities = models.CharField(max_length=500)
-    qualification = models.CharField(max_length=200)
+    company_description = models.TextField()
+    responsibilities = models.TextField()
+    qualification = models.TextField()
     experience = models.CharField(max_length=200)
-    skill = models.CharField(max_length=500)
-    leewayzon_facility = models.CharField(max_length=1000)
+    skill = models.TextField()
+    leewayzon_facility = models.TextField()
 
     def __str__(self):
         return self.job_position
@@ -28,14 +28,14 @@ class Application(models.Model):
 
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
+    email = models.EmailField(max_length=200) 
     address = models.CharField(max_length=300)
-    phone_number = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=10)
 
     experience_name = models.CharField(max_length=200)
     experience_company = models.CharField(max_length=200)
     experience_office_location = models.CharField(max_length=200)
-    experience_description = models.CharField(max_length=500)
+    experience_description = models.TextField()
     experience_date_from = models.DateField(blank=True, null=True)
     experience_date_to = models.DateField(blank=True, null=True)
 
@@ -43,18 +43,20 @@ class Application(models.Model):
     education_major = models.CharField(max_length=200)
     education_degree = models.CharField(max_length=200)
     education_school_location = models.CharField(max_length=200)
-    education_description = models.CharField(max_length=500)
+    education_description = models.TextField()
     education_date_from = models.DateField(blank=True, null=True)
     education_date_to = models.DateField(blank=True, null=True)
 
-    linkedin_link = models.URLField(max_length=1000, blank=True, null=True)
+    linkedin_link = models.URLField(max_length=1000, blank=True, null=True, unique=True)
     facebook_link = models.URLField(max_length=1000, blank=True, null=True)
     twitter_link = models.URLField(max_length=1000, blank=True, null=True)
     website_link = models.URLField(max_length=1000, blank=True, null=True)
 
     resume = models.FileField(upload_to='resumes/')
+    message = models.TextField()
 
-    message = models.CharField(max_length=1000)
+    class Meta:
+        unique_together = ('user', 'job')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
